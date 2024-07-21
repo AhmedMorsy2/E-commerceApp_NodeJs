08588bd528f49e2df4c2175726e3dddc924255ac
AhmedMorsy <20192234@fue.edu.eg>
2024-07-21 04:33:30 +0300

    main

--- Database/Models/user.model.js
+++ Database/Models/user.model.js
@@ -1,5 +1,5 @@
 import { Schema, model } from "mongoose";
-
+import bcrypt from "bcrypt";
 const schema = new Schema(
   {
     name: {
@@ -34,6 +34,13 @@ const schema = new Schema(
   }
 );
 
+schema.pre("save", function () {
+  this.password = bcrypt.hashSync(this.password, 8);
+});
 
+schema.pre("findOneAndUpdate", function () {
+  if (this._update.password)
+    this._update.password = bcrypt.hashSync(this._update.password, 8);
+});
 
 export const User = model("User", schema);
